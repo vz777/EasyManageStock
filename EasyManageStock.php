40e40b1ae897f89e4f150cf76e9f428bc502d608
vz777 <vincentbourbon@hotmail.fr>
2023-02-24 21:00:56 +0100

    Init commit

---
+++ EasyManageStock.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace EasyManageStock;
+
+use Propel\Runtime\Connection\ConnectionInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ServicesConfigurator;
+use Symfony\Component\Finder\Finder;
+use Thelia\Install\Database;
+use Thelia\Module\BaseModule;
+
+class EasyManageStock extends BaseModule
+{
+    /** @var string */
+    const DOMAIN_NAME = 'easymanagestock';
+
+    /*
+     * You may now override BaseModuleInterface methods, such as:
+     * install, destroy, preActivation, postActivation, preDeactivation, postDeactivation
+     *
+     * Have fun !
+     */
+
+    /**
+     * Defines how services are loaded in your modules
+     *
+     * @param ServicesConfigurator $servicesConfigurator
+     */
+    public static function configureServices(ServicesConfigurator $servicesConfigurator): void
+    {
+        $servicesConfigurator->load(self::getModuleCode().'\\', __DIR__)
+            ->exclude([THELIA_MODULE_DIR . ucfirst(self::getModuleCode()). "/I18n/*"])
+            ->autowire(true)
+            ->autoconfigure(true);
+    }
+
+    /**
+     * Execute sql files in Config/update/ folder named with module version (ex: 1.0.1.sql).
+     *
+     * @param $currentVersion
+     * @param $newVersion
+     * @param ConnectionInterface $con
+     */
+    public function update($currentVersion, $newVersion, ConnectionInterface $con = null): void
+    {
+        $finder = Finder::create()
+            ->name('*.sql')
+            ->depth(0)
+            ->sortByName()
+            ->in(__DIR__.DS.'Config'.DS.'update');
+
+        $database = new Database($con);
+
+        /** @var \SplFileInfo $file */
+        foreach ($finder as $file) {
+            if (version_compare($currentVersion, $file->getBasename('.sql'), '<')) {
+                $database->insertSql(null, [$file->getPathname()]);
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,35 @@
+WIP : THIS MODULE IS UNDER DEVELOPPMENT AND CAN CONTAIN SOME BUG
+
+# Easy Manage Stock
+
+This module use one page to update stock for products and their pse
+
+## Installation
+
+### Manually
+
+* Copy the module into ```<thelia_root>/local/modules/``` directory and be sure that the name of the module is EasyManageStock.
+* Activate it in your thelia administration panel
+
+### Composer
+
+Add it in your main thelia composer.json file
+
+```
+composer require your-vendor/easy-manage-stock-module:~1.0
+```
+
+## Usage
+
+Go on the config page, and update stock as you want.
+
+## Hook
+
+If your module use one or more hook, fill this part. Explain which hooks are used.
+
+## Other ?
+
+This module is in alpha and need improvment.
+For example, we need to add success confirmation for the user on succes updating stock.
+We need translations too.
+Tests, Debug, Comments, and PR are Welcome
